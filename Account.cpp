d40fddd258c6ceae92e7115112e5e4dddaf7f21c
ZeKai Chong <yokai.czk@gmail.com>
2021-12-29 02:50:46 +1030

    Update Account

--- Account.cpp
+++ Account.cpp
@@ -1,3 +1,7 @@
 #include "Account.h"
 #include <iostream>
 using namespace std;
+
+Account::Account(): name{""}, address{""}, phone_number{""}, email{""}, password{""}, salary{0}, isAdmin{false}, dob{new Time(nullptr)}, join_date{new Time(nullptr)}, leave_date{new Time(nullptr)} {};
+
+Account::Account(std::string n, std::string ad, std::string p_n, std::string em, std::string pass, int sl, bool admin, Time* birth, Time* join, Time* leave): name{n}, address{ad}, phone_number{p_n}, email{em}, password{pass}, salary{sl}, isAdmin{admin}, dob{birth}, join_date{join}, leave_date{leave} {};
--- Account.h
+++ Account.h
@@ -5,15 +5,15 @@
 
 class Account{
 
-private:
+protected:
     std::string name, address, phone_number, email, password;
     int salary;
     bool isAdmin;
     Time* dob, *join_date, *leave_date;
 
 public:
-    Account(): name{""}, address{""}, phone_number{""}, email{""}, password{""}, salary{0}, isAdmin{false}, dob{new Time(nullptr)}, join_date{new Time(nullptr)}, leave_date{new Time(nullptr)} {};
-    Account(std::string n, std::string ad, std::string p_n, std::string em, std::string pass, int sl, bool admin, Time* birth, Time* join, Time* leave): name{n}, address{ad}, phone_number{p_n}, email{em}, password{pass}, salary{sl}, isAdmin{admin}, dob{birth}, join_date{join}, leave_date{leave} {};
+    Account();
+    Account(std::string n, std::string ad, std::string p_n, std::string em, std::string pass, int sl, bool admin, Time* birth, Time* join, Time* leave);
     
     //Getter
     std::string get_string(std::string n);
@@ -22,10 +22,10 @@ public:
     Time* get_time(std::string n);
 
     //Setter
-    void get_string(std::string n);
-    void get_salary();
-    void get_admin();
-    void get_time(std::string n);
+    void set_string(std::string n, string data);
+    void set_salary(int data);
+    void set_admin(bool data);
+    void set_time(std::string n);
 
 
 };
